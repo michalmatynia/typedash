45e413e6aaf19ee3b3929c9fd81c6405e31a5ca2
Michal <mmatynia@gmail.com>
2023-04-01 15:21:11 +0200

    CsdcSD

---
+++ src/pages/material-kit-pro-react-v2.0.0/landingpage.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from 'react'
+
+import Page from 'pages-sections/material-kit-pro-react-v2.0.0/pages/Authentication//SignIn/Illustration'
+
+// const dashboardRoutes = []
+
+export default function LandingPage(): ReactNode {
+  return (
+    <>
+      <Page />
+    </>
+  )
+}
